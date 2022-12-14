3e5276517fe547bab5f03eb0354e35691e3ccf32
Caitlin <cme369@nyu.edu>
2022-12-14 16:26:45 -0500

    basic rate limiter implemented with redis

---
+++ rate-limiter/rate-limiter.js
@@ -0,0 +1,48 @@
+const Express = require('express');
+const Redis = require('redis');
+
+const redis = new Redis();
+
+/*
+* Rate Limiter
+* @param
+* @param
+* @param
+
+*/
+
+/* ON scratch logic
+  params: req res next
+
+  get ip address from either the proxy if there is one or the querier itself if none
+  ex (VERIFY): const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress
+
+  using ip address as a key, check if requester has a session active
+    if not, create one as a new property of the redis db with a set expiration of one minute
+    if so, increment # of queries associated with the ip address
+    **NOTE: ultimately this should not be a straight increment - we would incorporate cost analysis
+    and incrimate by the complexity "score" of the query**
+      if queries > limit, refuse query and through new graphqlerror (LOOK UP)
+      else return next()
+
+*/
+
+async function rateLimiter(req, res, next) {
+  const ip = req.headers['x-forwarded-for'] || req.connection.remoteAddress;
+  // if the ip address is in our redis store increment the count, otherwise initialize key val pair
+  const requestCount = await redis.incr(ip);
+  if (requestCount === 1) {
+    redis.expire(ip, 60);
+  }
+
+  // 60 here is an arbitrary number that will ultimately be replaced with a variable tied to config
+  if (requestCount > 60) {
+    return next({
+      message: 'Too Many Requests',
+      error: 429,
+      log: 'KnightOwl: Exceeded request limit, Error in rateLimiter.',
+    });
+  }
+
+  return next();
+}
